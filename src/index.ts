19f406ee6aea1bc2b9375312a458a1628e3fa387
chenjiahan <chenjiahan.jait@bytedance.com>
2024-07-17 20:54:00 +0800

    feat: add filename option

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,24 @@ import { resolveHttpsConfig } from './util.js';
 
 export const PLUGIN_BASIC_SSL_NAME = 'rsbuild:basic-ssl';
 
-export function pluginBasicSsl(): RsbuildPlugin {
-	return {
-		name: PLUGIN_BASIC_SSL_NAME,
-		setup(api) {
-			api.modifyRsbuildConfig((config) => {
-				config.server = {
-					...config.server,
-					https: resolveHttpsConfig(config.server?.https),
-				};
-			});
-		},
-	};
-}
+export type PluginBasicSslOptions = {
+	/**
+	 * Filename of the generated certificate
+	 * @default 'fake-cert.pem'
+	 */
+	filename?: string;
+};
+
+export const pluginBasicSsl = (
+	options: PluginBasicSslOptions = {},
+): RsbuildPlugin => ({
+	name: PLUGIN_BASIC_SSL_NAME,
+	setup(api) {
+		api.modifyRsbuildConfig((config) => {
+			config.server = {
+				...config.server,
+				https: resolveHttpsConfig(config.server?.https, options),
+			};
+		});
+	},
+});
---
+++ test/filename/rsbuild.config.ts
@@ -0,0 +1,5 @@
+import { pluginBasicSsl } from '@rsbuild/plugin-basic-ssl';
+
+export default {
+	plugins: [pluginBasicSsl()],
+};
